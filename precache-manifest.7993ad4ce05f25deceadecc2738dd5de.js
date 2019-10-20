d3296c4270c689008a5c842ae3d92edbdb16b6a1
Shashank Gupta <shashankcm90@gmail.com>
2019-10-20 12:43:46 -0400

    Updates

---
+++ precache-manifest.7993ad4ce05f25deceadecc2738dd5de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400632e0b687c2bee4e8e41ab3f28b00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36ee1ea90670f7734ca8",
+    "url": "/monsters-rolodex/static/css/main.e7e51795.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/monsters-rolodex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "36ee1ea90670f7734ca8",
+    "url": "/monsters-rolodex/static/js/main.7f7bb8bb.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
